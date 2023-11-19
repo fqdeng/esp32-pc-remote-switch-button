7b1df74193d39e7042c8be1f0254836a3d49afa8
fqdeng <fqdeng@trip.com>
2023-11-19 08:27:20 +0800

    finish

--- main/main.c
+++ main/main.c
@@ -1,14 +1,23 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
 #include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "http_server.h"
 
 void app_main(void)
 {
-    int i = 0;
-    while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
+
+
+    start_http_server();
+
 }
---
+++ main/wifi_connect.h
@@ -0,0 +1,10 @@
+//
+// Created by winters on 2023/11/19.
+//
+
+#ifndef APP_TEMPLATE_WIFI_CONNECT_H
+#define APP_TEMPLATE_WIFI_CONNECT_H
+
+#endif //APP_TEMPLATE_WIFI_CONNECT_H
+
+void connect_wifi(void);
